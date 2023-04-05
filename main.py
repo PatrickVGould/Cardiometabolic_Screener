11e5b6fd7a910a00708bb2a0b845d7cd8a946bd9
PatrickVGould <125094430+PatrickVGould@users.noreply.github.com>
2023-04-05 21:40:02 +1000

    changes

--- main.py
+++ main.py
@@ -11,8 +11,9 @@ def h4_divider(text):
     st.markdown("<h4 style='text-align: left; color: #404040;'>%s</h4>" % text, unsafe_allow_html=True)
 
 # Demographic questions
-st.header("""Demographics""")
-st.markdown("---")
+#st.header("""Demographics""")
+st.markdown("""Demographics
+---""")
 st.markdown("<style>div.stMarkdown {margin: 0; padding: 0;}</style>", unsafe_allow_html=True)
 patient_name = st.text_input(label = "What is the person's name?", help = "This will be used to personalise the report at the end and will not be stored or shared with anyone else.")
 col1, col2, col3 = st.columns(3)
