62d7d61837fb725d35f5f809b8f0e697ab4e70b1
PatrickVGould <patrick.gould@unsw.edu.au>
2023-06-05 08:15:20 +1000

    Update main.py

--- main.py
+++ main.py
@@ -245,8 +245,8 @@ if submit:
         <li>Consider metformin and/or GLP receptor agonist</li>""", unsafe_allow_html = True)
         if bmi in ["30-34","≥35"]:
             st.markdown(body = """Consider intensive intervention if BMI is greater than or equal to 30""", unsafe_allow_html = True)
-        st.markdown(body = """#### *Targets:*
-        <li>BMI 20-24.9 kg/m2 (<23 kg/m2) </li>
+        st.markdown(body = """#### *Targets:*""", unsafe_allow_html = True)
+        st.markdown(body = """<li>BMI 20-24.9 kg/m2 (<23 kg/m2) </li>
         <li>Waist circumference: <94 cm male (<90 cm) and <80 cm female </li>""", unsafe_allow_html = True)
 
     if systolic_bp == '≥140 mmHg' or diastolic_bp == '≥90 mmHg':
